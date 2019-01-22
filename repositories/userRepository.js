63b3136dc8dbe9ef211f5d38d19e708379d53b6f
willy1669 <williamschinonso11@gmail.com>
2019-01-22 23:49:40 +0100

    created userRepository

---
+++ repositories/userRepository.js
@@ -0,0 +1,9 @@
+var model = require('../models/User');
+var BaseRepository = require('../repositories/BaseRepository');
+
+function UserRepository(){
+
+}
+UserRepository.prototype = BaseRepository(model);
+
+module.exports = new UserRepository();
