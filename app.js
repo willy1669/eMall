007a1e1ed74f2faca5b8cc1fb07b7233ede9bc54
willy1669 <williamschinonso11@gmail.com>
2019-01-23 00:53:08 +0100

    created a mongoose connection  for my local database connectivity

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/eMall', { useNewUrlParser: true});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
