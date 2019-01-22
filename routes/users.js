4752ca2515a94f8f76488127bb579267925967cb
willy1669 <williamschinonso11@gmail.com>
2019-01-23 00:56:17 +0100

    created the users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/UserController');
+
 
 /* GET users listing. */
+router.post('/create', userController.addUser);
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
