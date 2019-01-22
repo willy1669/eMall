4ff59ee55a870aaa5c022a505b52da5eae35e0f5
willy1669 <williamschinonso11@gmail.com>
2019-01-22 23:48:14 +0100

    created baseRepository for addUser function

---
+++ repositories/baseRepository.js
@@ -0,0 +1,8 @@
+function BaseRepository(model){
+    if(!model) throw new Error('A model must be provided');
+    this.model = model;
+}
+
+BaseRepository.prototype.add = function(data, callback){
+    this.model.create(data, callback);
+}
