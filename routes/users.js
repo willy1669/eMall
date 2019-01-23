740443b635e02350687e6a15ddbfecb69e800d9d
willy1669 <williamschinonso11@gmail.com>
2019-01-24 00:48:35 +0100

    modified name

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/UserController');
+var userController = require('../controllers/UserControllers');
 
 
 /* GET users listing. */
