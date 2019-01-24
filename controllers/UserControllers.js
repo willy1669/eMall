8dfa220c1f4a57c80a3c442343cbfbc732e52be0
willy1669 <williamschinonso11@gmail.com>
2019-01-24 23:25:45 +0100

    added login function

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,13 +1,17 @@
 const User = require('../models/User');
 const Joi = require('joi');
 const passwordHash = require('password-hash');
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const secret = require('../configs/Secret');
 const service = require('../services/UserServices');
 
 const schema = Joi.object().keys({
     username: Joi.string().min(3).max(30).required(),
     password: Joi.string().required(),
     email: Joi.string().email()
-});
+}); 
 
 exports.addUser = function(req, res){
     var data = {
@@ -15,7 +19,7 @@ exports.addUser = function(req, res){
         email: req.body.email,
         password: req.body.password,
     };
-Joi.validate({username:data.username, email:data.email, password:data.password}, schema, function (err){
+Joi.validate({username:data.username, email:data.email, password:data.password}, schema, function (err) {
         if (err){
             return res.json ({err:err.message});
         } 
@@ -35,4 +39,44 @@ Joi.validate({username:data.username, email:data.email, password:data.password},
 
 function isValidPassword(user, password){
     return passwordHash.verify(password, user.password);
-  }
+}
+
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+exports.loginUser = function(req, res) {
+    passport.authenticate('login', {
+        successRedirect: '/users',
+        failureRedirect: '/login'
+    });
+    try {
+        passport.use('login', new LocalStrategy(
+            User.findOne({email: req.body.email}, function (err, user) { 
+                if (err) {
+                    res.json({err: err});
+                } 
+                if (user && isValidPassword(user, req.body.password)) {
+                    var token = jwt.sign({email: user.email, id: user._id}, secret.key, {expiresIn: '12h'});
+                    res.json({ userId:user._id, email:user.email, username: user.username, token: token, message: 'Login successful.'});
+                } 
+                else {
+                    res.json({ message: 'Incorrect username or password.' });
+                    console.log(isValidPassword (req.body.password));
+                }; 
+                console.log(user)
+            }), function(email, password, done) {
+                console.log(email);
+            }))
+        } 
+        catch(exception) {
+            console.log(exception);
+        }
+    }
+
