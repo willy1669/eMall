864d28743b03111c4a2b66c25f595852159218d2
bukkysos <33374240+willy1669@users.noreply.github.com>
2019-01-24 01:15:59 +0100

    deleted a module

--- repositories/userRepository.js
+++
@@ -1,9 +0,0 @@
-var model = require('../models/User');
-var BaseRepository = require('./BaseRepository');
-
-function UserRepository(){
-
-}
-UserRepository.prototype = BaseRepository(model);
-
-module.exports = new UserRepository();
